161c584e2bc35278477ca44721d9317809363834
devryu <devfbryu@gmail.com>
2020-10-10 20:41:47 +0900

    Second code with VPC

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,3 +1,27 @@
 resource "aws_vpc" "main"{
     cidr_block = "10.0.0.0/16"
+    
+    tags = {
+        Name = "IaC-vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/24"
+
+    availability_zone = "ap-northeast-2a"
+
+    tags = {
+        Name = "IaC-vpc-public-subnet"
+    }
+}
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.10.0/24"
+
+    tags = {
+        Name = "IaC-vpc-private-subnet"
+    }
 }
