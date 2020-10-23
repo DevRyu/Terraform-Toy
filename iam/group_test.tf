3adaab151757652b76a62881cdc9d0e996605108
devryu <devfbryu@gmail.com>
2020-10-24 06:09:20 +0900

    Add Iam test.user To Iam Group

--- iam/group_test.tf
+++ iam/group_test.tf
@@ -1,3 +1,11 @@
 resource "aws_iam_group" "test_group" {
     name = "test.group"
 } 
+
+resource "aws_iam_group_membership" "test_group" {
+    name = aws_iam_group.test_group.name
+    users =  [
+    aws_iam_user.test_user.name
+]
+    group = aws_iam_group.test_group.name
+}
