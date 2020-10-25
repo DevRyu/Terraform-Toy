b9a4993d099ce2112551aac182d8deb303ed25c1
devryu <devfbryu@gmail.com>
2020-10-26 00:18:53 +0900

    Add aws_iam_user_policy

--- iam/user_test.tf
+++ iam/user_test.tf
@@ -1,3 +1,24 @@
 resource "aws_iam_user" "test_user" {
     name = "test.user"
 } 
+resource "aws_iam_user_policy" "super-user-policy" {
+  name  = "super-admin"
+  user  = aws_iam_user.test_user.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
