f2fb5cf747014648f82f721a3a204e2ca4dd3f04
devryu <devfbryu@gmail.com>
2020-10-25 23:32:03 +0900

    Add iam_role_first time

---
+++ iam/iam_role_first.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "first" {
+  name               = "hello-iam-first"
+  path               = "/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
