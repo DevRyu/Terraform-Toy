b308bfc2e496e1fa26ea6b950f874472e2751726
devryu <devfbryu@gmail.com>
2020-10-26 00:19:09 +0900

    Reset aws_iam_user_policy

--- iam/user_test.tf
+++ iam/user_test.tf
@@ -1,24 +1,3 @@
 resource "aws_iam_user" "test_user" {
     name = "test.user"
 } 
-resource "aws_iam_user_policy" "super-user-policy" {
-  name  = "super-admin"
-  user  = aws_iam_user.test_user.name
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "*"
-            ],
-            "Resource": [
-                "*"
-            ]
-        }
-    ]
-}
-EOF
-}
