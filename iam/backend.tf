6794224954bfd943ae2e892df756a9e6d91ccfb3
devryu <devfbryu@gmail.com>
2020-10-28 08:26:00 +0900

    update profile in iam init.tf

--- iam/backend.tf
+++ iam/backend.tf
@@ -1,9 +1,10 @@
 terraform {
-    backend "s3" { # 강의는 
-      bucket         = "test-devryu-tfstate" # s3 bucket 이름
-      key            = "Users/ryu/tf/iam/terraform.tfstate" # s3 내에서 저장되는 경로를 의미합니다.
+    backend "s3" {
+      bucket         = "test-devryu-tfstate" 
+      key            = "tf/iam/terraform.tfstate" 
       region         = "ap-northeast-2"  
       encrypt        = true
       dynamodb_table = "terraform-lock"
+      profile = "my_aws"
     }
 }
